86c5782733983ee098c44e8d9231db143cd85824
Heitor Botelho <heitorbotelhotupi@gmail.com>
2025-05-01 23:56:33 -0300

    Add files via upload

---
+++ Mundo_1/exercício_3.py
@@ -0,0 +1,10 @@
+#Crie um programa que leia dois números e mostre a soma entre eles.
+
+num_1=int(input("Digite o primeiro número da soma: "))
+print()
+num_2=int(input("Digite o segundo número da soma: "))
+print()
+
+resultado = num_1 + num_2
+
+print("O resultado da soma desses números é:",resultado)
